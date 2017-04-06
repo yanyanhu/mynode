3834454755841d7fe52a6b7bba82d8ada6c72f62
YanyanHu <yanyanhu@cn.ibm.com>
2017-04-06 03:58:06 +0000

    Try nightmare

--- test/travis/scripts/test.sh
+++ test/travis/scripts/test.sh
@@ -6,3 +6,39 @@ docker run --name test-busybox -idt busybox:latest httpd -f -p 3000
 docker ps -a
 docker rm -f test-busybox
 echo "docker test finished."
+
+function getdir(){
+    for element in `ls $1`
+    do
+        dir_or_file=$1"/"$element
+        if [ -d $dir_or_file ]
+        then
+            getdir $dir_or_file
+        else
+            echo $dir_or_file
+        fi
+    done
+}
+
+echo "Install nightmare"
+cd ../..
+npm install nightmare
+echo "Done"
+
+root_dir="./unit"
+echo Running unit test...
+for filename in `getdir $root_dir`
+do
+    echo Running test case $filename ...
+    xvfb-run --server-args="-screen 0 1024x768x24" node $filename
+done
+echo Running unit test, done.
+
+root_dir="./e2e"
+echo Running end-to-end test...
+for filename in `getdir $root_dir`
+do
+    echo Running test case $filename ...
+    xvfb-run --server-args="-screen 0 1024x768x24" node $filename
+done
+echo Running end-to-end test, done.
