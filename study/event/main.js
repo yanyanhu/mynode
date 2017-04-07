f0d334a815634312ddc907d934b67d02358929c7
YanyanHu <yanyanhu@cn.ibm.com>
2017-04-07 05:14:01 +0000

    Study event

---
+++ study/event/main.js
@@ -0,0 +1,40 @@
+var events = require("events")
+var eventEmitter = new events.EventEmitter()
+
+
+var connectHandler = function connected() {
+    console.log("Connection building succeeded.")
+    setTimeout (function() {
+        eventEmitter.emit("data_received", "UserA")
+    }, 3000)
+}
+
+eventEmitter.on("connection", connectHandler)
+
+data_receiver1 = function(user) {
+    console.log(user)
+    console.log("Data receiving succeeded.")
+}
+
+data_receiver2 = function() {
+    console.log("Data receiving succeeded too.")
+}
+
+eventEmitter.on("data_received", data_receiver1)
+eventEmitter.addListener("data_received", data_receiver2)
+
+numListeners1 = events.EventEmitter.listenerCount(eventEmitter, "connection")
+numListeners2 = events.EventEmitter.listenerCount(eventEmitter, "data_received")
+console.log(numListeners1, numListeners2)
+
+eventEmitter.emit("connection")
+
+setTimeout(function() {
+    console.log("Remove one listener on data_received")
+    eventEmitter.removeListener("data_received", data_receiver2)
+    numListeners1 = events.EventEmitter.listenerCount(eventEmitter, "connection")
+    numListeners2 = events.EventEmitter.listenerCount(eventEmitter, "data_received")
+    console.log(numListeners1, numListeners2)
+    eventEmitter.emit("connection")
+}, 10000)
+
