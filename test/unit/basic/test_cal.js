2c8d9be1a0221ea26d321f6fa623e0d404f03374
YanyanHu <yanyanhu@cn.ibm.com>
2017-04-19 06:48:32 +0000

    Fix an error in test_cal

--- test/unit/basic/test_cal.js
+++ test/unit/basic/test_cal.js
@@ -5,20 +5,20 @@ var cal = rewire("study/basic/cal.js");
 describe("cal", function() {
   describe("add", function() {
     it("Should return the sum of two input value.", function() {
-      add = cal.__get__("add");
+      const add = cal.__get__("add");
       assert.equal(3, add(1, 2));
     });
 
     it("Should always return mock value", function() {
-      mock_add = function(a, b) {
+      const mock_add = function(a, b) {
         return 0;
       };
-      mock_add2 = function(a, b) {
+      const mock_add2 = function(a, b) {
         return 1;
       };
       cal.__set__("add", mock_add);
       assert.equal(0, cal.Add(1, 2));
-      cal.__set__("add", mock_add);
+      cal.__set__("add", mock_add2);
       assert.equal(1, cal.Add(1, 2));
     });
   });
