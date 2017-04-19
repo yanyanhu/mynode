30540ec0f36219f68d40f4c21d2a8fea292e4f5e
YanyanHu <yanyanhu@cn.ibm.com>
2017-04-19 03:23:11 +0000

    Try more on prettier

--- test/unit/basic/test_cal.js
+++ test/unit/basic/test_cal.js
@@ -1,21 +1,25 @@
-var assert = require('assert')
-var rewire = require('rewire')
-var cal = rewire('study/basic/cal.js')
+var assert = require("assert");
+var rewire = require("rewire");
+var cal = rewire("study/basic/cal.js");
 
+describe("cal", function() {
+  describe("add", function() {
+    it("Should return the sum of two input value.", function() {
+      add = cal.__get__("add");
+      assert.equal(3, add(1, 2));
+    });
 
-describe('cal', function() {
-    describe('add', function () {
-        it('Should return the sum of two input value.', function() {
-            add = cal.__get__('add')
-            assert.equal(3, add(1, 2))
-        })
-
-        it('Should always return 0', function () {
-            mock_add = function(a, b) {
-                return 0
-            }
-            cal.__set__('add', mock_add)
-            assert.equal(0, cal.Add(1, 2))
-        })
-    })
-})
+    it("Should always return mock value", function() {
+      mock_add = function(a, b) {
+        return 0;
+      };
+      mock_add2 = function(a, b) {
+        return 1;
+      };
+      cal.__set__("add", mock_add);
+      assert.equal(0, cal.Add(1, 2));
+      cal.__set__("add", mock_add);
+      assert.equal(1, cal.Add(1, 2));
+    });
+  });
+});
